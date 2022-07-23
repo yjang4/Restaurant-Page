8ffbc2a5b3e2047e8dca8945458abbaa0190ebe9
Daniel <mikyjo2012@gmail.com>
2022-07-23 11:24:48 -0500

    d

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ const page = () => {
     const content = document.getElementById("content");
     function populateContent() {
         let div = document.createElement("div");
+        let btn = document.createElement("button");
         div.textContent = "HAHAHAHHH";
+        btn.textContent = "menu";
+        addBtnListener(btn);
         content.appendChild(div);
+        content.appendChild(btn);
+    }
+    function addBtnListener(btn) {
+        btn.addEventListener("click", function() {
+            removeContent();
+            populateMenu();
+        });
     }
     function removeContent() {
         while (content.firstChild) {
